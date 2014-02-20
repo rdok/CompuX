52e86e41cf5c0f82120ed6e805810a0fc2e8e966
Rizart Dokollari <r.dokollari@gmail.com>
2014-02-20 11:36:34 +0200

    Update config.php
    Removed database credentials. Safety.

--- inc/config.php
+++ inc/config.php
@@ -5,11 +5,13 @@
 	define("BASE_URL","/compux_local/");
 	define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/compux_local/");
 
-	define("DB_HOST", "compux-db.rizartdokollari.com");
-	define("DB_NAME", "compux_db");
-	define("DB_PORT", "3306"); // default port.
-	define("DB_USER", "rizdok");
-	define("DB_PASS", "dzhxIIsnVd8dsHymbH6h");
+	// removed for safety reasons.
+	
+	define("DB_HOST", "");
+	define("DB_NAME", "");
+	define("DB_PORT", ""); // default port.
+	define("DB_USER", "");
+	define("DB_PASS", "");
 
 	// mail constants
 	define("CONTACT_EMAIL", "r.dokollari@acg.edu");
